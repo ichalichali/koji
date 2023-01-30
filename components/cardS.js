cba70bc7d6c7af05be6b6adbdaaf69c91023f28d
ichalichali <79679386+ichalichali@users.noreply.github.com>
2023-01-30 08:16:27 +0700

    commit

--- components/cardS.js
+++ components/cardS.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
-function SmallCard(id, img, location, distance) {
-  return <div>cardS</div>;
+function SmallCard({ id, img, location, distance }) {
+  return (
+    <div className="flex items-center m-2 space-x-2 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
+      {/*left */}
+      <div className="relative h-24 w-24 ">
+        <Image src={img} fill className=" rounded-lg shadow-sm" />
+      </div>
+
+      {/*Right */}
+      <div>
+        <h2> {location}</h2>
+        <h3> {distance}</h3>
+      </div>
+    </div>
+  );
 }
 
 export default SmallCard;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/banner";
-import SmallCard from "../components/cardS"
+import SmallCard from "../components/cardS";
 
 export default function Home({ exploreData }) {
   return (
@@ -15,31 +15,30 @@ export default function Home({ exploreData }) {
       <Banner />
 
       <main>
-        <section>
-          <h2 className=" text-4xl font-bold p-10">Explore Near By</h2>
-
-          {exploreData.map(post =>
-          <SmallCard 
-          key={post.id}
-          img={post.img}
-          distance={post.distance}
-          location={post.location}
-          />
-          )}
-  
+        <section className="p-10">
+          <h2 className=" text-4xl font-bold py-10">Explore Near By</h2>
+          <div className="grid grid-cols-1 sm: grid-cols-2 lg: grid-cols-3 xl:grid-cols-4">
+            {exploreData.map((post) => (
+              <SmallCard
+                key={post.id}
+                img={post.img}
+                distance={post.distance}
+                location={post.location}
+              />
+            ))}
+          </div>
         </section>
       </main>
     </div>
   );
 }
 
-import fsPromises from 'fs/promises';
-import path from 'path'
+import fsPromises from "fs/promises";
+import path from "path";
 export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), '/data/room.json');
+  const filePath = path.join(process.cwd(), "/data/room.json");
   const jsonData = await fsPromises.readFile(filePath);
   const exploreData = JSON.parse(jsonData);
-  
 
   return {
     props: {
